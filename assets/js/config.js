bdf741093c3867b8f1728a9faa229e1701feea55
sdhami0071-ai <s.dhami00712@gmail.com>
2026-04-23 11:43:14 +0530

    Update backend URL to fix CORS error

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,3 @@
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyN_vH_-N_60x0602kP_2q6Z-q_g1060_e2Kz-2LwA/exec'; // Need to be replaced by the user after deploying
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzMoKg38kXwrzOe6gbFg1E5AXhc6cSquZLWkJ8uWm0OQqa9PbjWQVAYoPwPWSA-lBhgug/exec';
 const TEST_DURATION_SECONDS = 90 * 60; // 90 minutes
 const TOTAL_QUESTIONS = 100;
